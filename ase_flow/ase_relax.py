05c4c43d0b7950e411486548b993a52b4b7ef67f
Wei Chen <wchen@frontal01.lucia.cenaero.be>
2025-06-05 20:28:23 +0200

    Fix the ideal gas mode for free-energy calculations

--- ase_flow/ase_relax.py
+++ ase_flow/ase_relax.py
@@ -1,44 +1,82 @@
 from jobflow import job
 from ase import Atoms
-from ase.optimize import BFGS, FIRE
-from ase.io import read, write
+from ase.optimize import BFGS, LBFGS, FIRE
+from ase.optimize.precon import PreconLBFGS
+from ase.constraints import FixAtoms
+from ase.io import read, write, Trajectory
 from pymatgen.io.ase import AseAtomsAdaptor
 from ase_flow.vibrations import calc_free_energy
 import os, socket
 
+
 def mace_calculator(atoms, model):
     from mace.calculators import MACECalculator
+
     if model is None:
-         model = "/gpfs/scratch/acad/htbase/wchen/mxene/mlip/pbe0-rvv10_8/Ti/train/mace-omat-0-medium.model"
-    calc = MACECalculator(model_paths=model, device='cuda')
+        model = "/gpfs/scratch/acad/htbase/wchen/mxene/mlip/pbe0-rvv10_8/Ti/train/mace-omat-0-medium.model"
+    calc = MACECalculator(model_paths=model, device="cuda")
     atoms.calc = calc
     return atoms
+
 
 def orb_calculator(atoms, model):
     from orb_models.forcefield.calculator import ORBCalculator
     from orb_models.forcefield import pretrained
+
     if model is None:
-        model = pretrained.orb_v3_conservative_inf_omat(device='cuda', precision="float32-high")
-    calc = ORBCalculator(model, device='cuda')
+        model = pretrained.orb_v3_conservative_inf_omat(
+            device="cuda", precision="float32-high"
+        )
+    calc = ORBCalculator(model, device="cuda")
     atoms.calc = calc
     return atoms
 
+
 @job
-def relax_ase(path_to_structure="POSCAR", atoms=None, forcefield="mace", model=None, alias='', free_energy=True, mode_fe='harmonic'):
+def relax_ase(
+    path_to_structure="POSCAR",
+    atoms=None,
+    fix_below=None,
+    rattle=None,
+    forcefield="mace",
+    model=None,
+    alias="",
+    free_energy=True,
+    mode_fe="harmonic",
+    temperature=298.15,
+    pressure=101325,
+    geometry="linear",
+    symmetrynumber=2,
+    spin=0
+):
     if atoms is not None:
         atoms = AseAtomsAdaptor.get_atoms(atoms)
     else:
         atoms = read(path_to_structure)
-    
+
     if forcefield.lower() == "mace":
         atoms = mace_calculator(atoms, model)
     elif forcefield.lower() == "orb":
         atoms = orb_calculator(atoms, model)
 
+    if fix_below:
+        fixed_indices = [i for i, pos in enumerate(atoms.positions) if pos[2] < fix_below]
+        atoms.set_constraint(FixAtoms(indices=fixed_indices))
+
+    if rattle is not None:
+        atoms.rattle(stdev=rattle, seed=100)
+
     initial_atoms = atoms.copy()
     write("POSCAR", initial_atoms)
-    dyn = FIRE(atoms, logfile="opt.log")
-    dyn.run(fmax=0.01)
+    """opt1 = PreconLBFGS(atoms, logfile="opt1.log")
+    traj = Trajectory('opt1.traj', 'w', atoms)
+    opt1.attach(traj.write, interval=10)
+    opt1.run(fmax=0.02)"""
+    #
+    opt = PreconLBFGS(atoms, logfile="opt.log")
+    traj = Trajectory("opt.traj", "w", atoms)
+    opt.attach(traj.write, interval=10)
+    opt.run(fmax=0.0095)
     write("CONTCAR", atoms)
 
     initial_structure = AseAtomsAdaptor.get_structure(initial_atoms)
@@ -47,18 +85,25 @@ def relax_ase(path_to_structure="POSCAR", atoms=None, forcefield="mace", model=N
 
     output = {
         "output": {
-        "structure": final_structure.as_dict(),
-        "final_structure": final_structure.as_dict(),
-        "energy": energy,
-        "final_energy": energy,
-        "energy_per_atom": energy / len(final_structure)
+            "structure": final_structure.as_dict(),
+            "final_structure": final_structure.as_dict(),
+            "energy": energy,
+            "final_energy": energy,
+            "energy_per_atom": energy / len(final_structure),
         }
     }
 
     if free_energy:
-        free_energy = calc_free_energy(atoms, temperature=298.15, pressure = 101325, mode=mode_fe, 
-            geometry='nonlinear', symmetrynumber=2, spin=0)
-        output["output"]["free_energy"] = free_energy    
+        free_energy = calc_free_energy(
+            atoms,
+            temperature=temperature,
+            pressure=pressure,
+            mode=mode_fe,
+            geometry=geometry,
+            symmetrynumber=symmetrynumber,
+            spin=spin,
+        )
+        output["output"]["free_energy"] = free_energy
 
     results = {
         "dir_name": f"{socket.gethostname()}:{os.getcwd()}",
@@ -66,10 +111,8 @@ def relax_ase(path_to_structure="POSCAR", atoms=None, forcefield="mace", model=N
         "nsites": len(final_structure),
         "formula_pretty": final_structure.composition.reduced_formula,
         "composition": final_structure.composition.as_dict(),
-        "input": {
-            "calculator": atoms.calc.name
-        },
-        "output": output
+        "input": {"calculator": atoms.calc.name},
+        "output": output,
     }
 
     return results
--- ase_flow/workflow.py
+++ ase_flow/workflow.py
@@ -5,59 +5,136 @@ from pymatgen.io.vasp import Poscar
 from ase.build import molecule, add_adsorbate
 from ase import Atom
 from ase_flow.ase_relax import relax_ase
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import numpy as np
 import os
+
 
 @job
 def load_surface(filename="POSCAR"):
     structure = Poscar.from_file(filename).structure
     return structure
 
+
 @job
-def adsorb(surface_structure, adsorbate, height, position):
+def adsorb(surface_structure, adsorbate, rotation, height, position):
     slab = AseAtomsAdaptor.get_atoms(surface_structure)
     if isinstance(adsorbate, str) and os.path.isfile(adsorbate):
         adsorbate = read(adsorbate)
     else:
         adsorbate = molecule(adsorbate)
-
+    adsorbate.rotate(rotation["x"], "x")
+    adsorbate.rotate(rotation["y"], "y")
+    adsorbate.rotate(rotation["z"], "z")
     add_adsorbate(slab, adsorbate, height=height, position=position)
     return AseAtomsAdaptor.get_structure(slab)
 
-@job 
-def protonate(surface_structure, anchor_site: int, height=1.0, offset=(0.0,0.0)):
+
+@job
+def protonate(surface_structure, anchor_site: int, height=1.0, offset=(0.0, 0.0)):
     slab = AseAtomsAdaptor.get_atoms(surface_structure)
     anchor_pos = slab.positions[anchor_site]
     h_pos = anchor_pos + np.array([offset[0], offset[1], height])
     slab.append(Atom("H", h_pos))
     return AseAtomsAdaptor.get_structure(slab)
-    
+
+
+@job
+def remove_indices(surface_structure, indices_to_remove):
+    slab = AseAtomsAdaptor.get_atoms(surface_structure)
+    for i in sorted(indices_to_remove, reverse=True):
+        del slab[i]
+    return AseAtomsAdaptor.get_structure(slab)
+
+
+@dataclass
+class RelaxWorkflow:
+    structure: object = None
+    filename: str = "POSCAR"
+    fix_below: float = None
+    forcefield: str = "mace"
+    rattle: float = None
+    model: str = None
+    alias: str = ""
+    free_energy: bool = True
+    mode_fe: str = "harmonic"
+    temperature: float = 298.15
+    pressure: float = 101325
+    geometry: str = "linear"
+    symmetrynumber: int = 2
+    spin: int = 0
+    job_name: str = "substrate relax"
+
+    def build(self):
+        if self.structure is not None:
+            structure = self.structure
+            load = None
+        else:
+            load = load_surface(filename=self.filename)
+            structure = load.output
+        relax = relax_ase(
+            atoms=structure,
+            fix_below=self.fix_below,
+            forcefield=self.forcefield,
+            model=self.model,
+            alias=self.alias,
+            free_energy=self.free_energy,
+            mode_fe=self.mode_fe,
+            temperature=self.temperature,
+            pressure=self.pressure,
+            geometry=self.geometry,
+            symmetrynumber=self.symmetrynumber,
+            spin=self.spin
+        )
+        relax.name = self.job_name
+        jobs = [relax] if load is None else [load, relax]
+        return Flow(jobs), relax
+
+
 @dataclass
 class AdsorbateRelaxWorkflow:
     structure: object = None
     filename: str = "POSCAR"
     adsorbate: str = "CO2"
+    rotation: dict = field(default_factory=lambda: {"x": 0, "y": 0, "z": 0})
     height: float = 2.0
-    position: tuple = (0,0),
+    position: tuple = ((0, 0),)
+    fix_below: float = None
+    rattle: float = None
     forcefield: str = "mace"
     model: str = None
-    alias: str = ''
-    job_name: str = "adsorbate relax"
+    alias: str = ""
+    free_energy: bool = True
     mode_fe = "harmonic"
-    
+    job_name: str = "adsorbate relax"
+
     def build(self):
         if self.structure is not None:
             surface_structure = self.structure
             load = None
         else:
-            load = load_surface(filename = self.filename)
+            load = load_surface(filename=self.filename)
             surface_structure = load.output
-        add = adsorb(surface_structure, adsorbate = self.adsorbate, height=self.height, position=self.position)
-        relax = relax_ase(atoms=add.output, forcefield=self.forcefield, model=self.model, alias=self.alias, mode_fe=self.mode_fe)
+        add = adsorb(
+            surface_structure,
+            adsorbate=self.adsorbate,
+            rotation=self.rotation,
+            height=self.height,
+            position=self.position,
+        )
+        relax = relax_ase(
+            atoms=add.output,
+            fix_below=self.fix_below,
+            forcefield=self.forcefield,
+            model=self.model,
+            alias=self.alias,
+            free_energy=self.free_energy,
+            mode_fe=self.mode_fe,
+        )
         relax.name = self.job_name
         jobs = [add, relax] if load is None else [load, add, relax]
         return Flow(jobs), relax
+
 
 @dataclass
 class ProtonationRelaxWorkflow:
@@ -66,20 +143,76 @@ class ProtonationRelaxWorkflow:
     anchor_site: int = 0
     height: float = 1.0
     offset: tuple = (0.0, 0.0)
+    fix_below: float = None
+    rattle: float = None
     forcefield: str = "mace"
     model: str = None
-    alias: str = ''
+    alias: str = ""
+    free_energy: bool = True
+    mode_fe = "harmonic"
     job_name: str = "adsorbate relax"
-    
+
     def build(self):
         if self.structure is not None:
             surface_structure = self.structure
             load = None
         else:
-            load = load_surface(filename = self.filename)
+            load = load_surface(filename=self.filename)
             surface_structure = load.output
-        add = protonate(surface_structure, anchor_site=self.anchor_site, height=self.height, offset=self.offset)
-        relax = relax_ase(atoms=add.output, forcefield=self.forcefield, model=self.model, alias=self.alias)
+        add = protonate(
+            surface_structure,
+            anchor_site=self.anchor_site,
+            height=self.height,
+            offset=self.offset,
+        )
+        relax = relax_ase(
+            atoms=add.output,
+            fix_below=self.fix_below,
+            forcefield=self.forcefield,
+            model=self.model,
+            alias=self.alias,
+            free_energy=self.free_energy,
+            mode_fe=self.mode_fe
+        )
         relax.name = self.job_name
         jobs = [add, relax] if load is None else [load, add, relax]
+        return Flow(jobs), relax
+
+@dataclass
+class DesorbRelaxWorkflow:
+    structure: object = None
+    filename: str = "POSCAR"
+    indices_to_remove: list = None
+    fix_below: float = None
+    rattle: float = None
+    forcefield: str = "mace"
+    model: str = None
+    alias: str = ""
+    free_energy: bool = True
+    mode_fe = "harmonic"
+    job_name: str = "adsorbate relax"
+
+    def build(self):
+        if self.structure is not None:
+            surface_structure = self.structure
+            load = None
+        else:
+            load = load_surface(filename=self.filename)
+            surface_structure = load.output
+        desorb = remove_indices(
+            surface_structure,
+            indices_to_remove=self.indices_to_remove
+        )
+        relax = relax_ase(
+            atoms=desorb.output,
+            fix_below=self.fix_below,
+            rattle=self.rattle,
+            forcefield=self.forcefield,
+            model=self.model,
+            alias=self.alias,
+            free_energy=self.free_energy,
+            mode_fe=self.mode_fe,
+        )
+        relax.name = self.job_name
+        jobs = [desorb, relax] if load is None else [load, desorb, relax]
         return Flow(jobs), relax
